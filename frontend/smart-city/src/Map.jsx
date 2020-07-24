8ff05966fe901f431d3381ab3f941e642b4ba4f4
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-07-24 22:33:00 +0200

    Switch to react-leaflet

---
+++ frontend/smart-city/src/Map.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+const SmartCityMap = props => {
+  const [position, setPosition] = useState([52.217154, 21.005575]);
+  const [zoom, setZoom] = useState(15);
+
+  return (
+    <Map center={position} zoom={zoom}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
+  );
+};
+
+export default SmartCityMap;
