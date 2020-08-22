c054b27daee7febbf08228ab4648d0f53a75892a
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-08-22 13:34:03 +0200

    Add numberOfCars, testCarNumber and startVehicles to Menu. Send appropriate request to backend

--- frontend/src/web/ApiManager.js
+++ frontend/src/web/ApiManager.js
@@ -1,16 +1,26 @@
-import { SET_ZONE_REQUEST } from "./MessageType";
+import { SET_ZONE_REQUEST, START_SIMULATION_REQUEST } from "./MessageType";
 import WebServer from "./WebServer";
 
 /** PUBLIC INTERFACE ---------------------------------------------------------- */
 
 export default {
   setZone({ lat, lng, rad } = { lat: 0, lng: 0, rad: 0 }) {
-    var msg = {
+    const msg = {
       type: SET_ZONE_REQUEST,
       payload: {
         latitude: lat,
         longitude: lng,
         radius: rad,
+      },
+    };
+    WebServer.send(msg);
+  },
+  startVehicles({ carsNum, testCarNum } = { carsNum: 0, testCarNum: 0 }) {
+    const msg = {
+      type: START_SIMULATION_REQUEST,
+      payload: {
+        carsNum: carsNum,
+        testCarId: testCarNum,
       },
     };
     WebServer.send(msg);
