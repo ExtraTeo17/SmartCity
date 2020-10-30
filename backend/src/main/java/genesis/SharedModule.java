b4758dc7ed934b305476e4e95a8cfe27c455efef
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-10-30 01:02:19 +0100

    Server perfomance improvements.

--- backend/src/main/java/genesis/SharedModule.java
+++ backend/src/main/java/genesis/SharedModule.java
@@ -2,8 +2,10 @@ package genesis;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.inject.Inject;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
@@ -27,5 +29,12 @@ public class SharedModule extends AbstractModule {
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return mapper;
+    }
+
+    @Provides
+    @Singleton
+    @Inject
+    ObjectWriter getObjectMapper(ObjectMapper mapper) {
+        return mapper.writer();
     }
 }
