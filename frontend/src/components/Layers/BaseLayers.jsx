21778e502bda9d31f0d376cbc8a10721948fee9a
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-11-02 14:09:07 +0100

    Fix no key for base layers array
    (cherry picked from commit b6925fbc71467a47067bf539fb1d1b11f65db3bd)

--- frontend/src/components/Layers/BaseLayers.jsx
+++ frontend/src/components/Layers/BaseLayers.jsx
@@ -8,7 +8,7 @@ import { MAX_NATIVE_ZOOM, MAX_ZOOM } from "../../constants/minMax";
 const { BaseLayer } = LayersControl;
 
 export const BaseLayers = [
-  <BaseLayer checked name="Map - Main">
+  <BaseLayer checked name="Map - Main" key="mapMain">
     <TileLayer
       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -16,7 +16,7 @@ export const BaseLayers = [
       maxNativeZoom={MAX_NATIVE_ZOOM}
     />
   </BaseLayer>,
-  <BaseLayer name="Map - Black & White">
+  <BaseLayer name="Map - Black & White" key="mapBlack">
     <TileLayer
       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
@@ -24,7 +24,7 @@ export const BaseLayers = [
       maxNativeZoom={MAX_NATIVE_ZOOM}
     />
   </BaseLayer>,
-  <BaseLayer name="Map - Streets">
+  <BaseLayer name="Map - Streets" key="mapStreets">
     <TileLayer
       attribution='&amp;copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a> &amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=JTQMBGnY9XYwZnvQilbM"
@@ -32,7 +32,7 @@ export const BaseLayers = [
       maxNativeZoom={MAX_NATIVE_ZOOM}
     />
   </BaseLayer>,
-  <BaseLayer name="Map - Carto">
+  <BaseLayer name="Map - Carto" key="mapCarto">
     <TileLayer
       attribution='&amp;copy; <a href="https://carto.com/about-carto/">Carto</a> &amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png"
@@ -40,7 +40,7 @@ export const BaseLayers = [
       maxNativeZoom={MAX_NATIVE_ZOOM}
     />
   </BaseLayer>,
-  <BaseLayer name="Map - Here">
+  <BaseLayer name="Map - Here" key="mapHere">
     <TileLayer
       attribution='&amp;copy; <a href="http://here.net/services/terms">HERE 2020</a> &amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url={`https://2.aerial.maps.ls.hereapi.com/maptile/2.1/maptile/newest/${D_HERE_MAP_STYLE}/{z}/{x}/{y}/256/png8?apiKey=${HERE_MAP_API_KEY}`}
