b27c1267a3be2efad1f53a86c8adcacbc1015ca7
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-12-02 23:26:15 +0100

    Change route change point icon.

--- frontend/src/components/Markers/RouteChangePoint.jsx
+++ frontend/src/components/Markers/RouteChangePoint.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Marker, Popup } from "react-leaflet";
+import { Popup, CircleMarker } from "react-leaflet";
 import { STATIC_Z_INDEX } from "../../constants/markers";
-import { goldMarkerIcon } from "../../styles/icons";
 
 const RouteChangePoint = props => {
   const { point } = props;
 
   return point ? (
-    <Marker position={point} icon={goldMarkerIcon} zIndexOffset={STATIC_Z_INDEX + 2}>
+    <CircleMarker
+      center={point}
+      radius={8}
+      color="yellow"
+      opacity={0.75}
+      fillColor="yellow"
+      fillOpacity={0.85}
+      zIndexOffset={STATIC_Z_INDEX + 2}
+    >
       <Popup>Route changed here!</Popup>
-    </Marker>
+    </CircleMarker>
   ) : null;
 };
 
