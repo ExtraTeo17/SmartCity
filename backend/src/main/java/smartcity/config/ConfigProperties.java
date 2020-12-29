fba0f19fc25b1b9bd7e99282dbd3beb03c5bf815
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-12-29 20:29:44 +0100

    Fix config location.

--- backend/src/main/java/smartcity/config/ConfigProperties.java
+++ backend/src/main/java/smartcity/config/ConfigProperties.java
@@ -3,6 +3,6 @@ package smartcity.config;
 
 // Overridden with properties
 public class ConfigProperties {
-    public static int JADE_PORT = 9000;
-    public static int WEB_PORT = 4000;
+    public static int JADE_PORT = 4000;
+    public static int WEB_PORT = 9000;
 }
