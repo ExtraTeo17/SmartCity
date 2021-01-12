73a4cb324e5bffeee80ce3790183592632158e45
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2021-01-12 04:47:54 +0100

    Fix and improve long_runner script. Add readme for script.

--- scripts/long_runner.sh
+++ scripts/long_runner.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-for i in {1..10}
-do
-    cd ..; 
+if [ "$1" != "" ]; then
+  runs=$1;
+else
+  runs=10
+fi
+echo "Runs = $runs";
+
+cd ..; 
+
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "-----------------------------------------------------------------------------------"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+i=0;
+while (( i < $runs )); do
     ./run.sh &
     pid=$!
     sleep 15; 
     node scripts/connect.js
     wait $pid
+    ((i++))
 done
 
+cd scripts;
